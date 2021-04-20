911fda1c07502447002f11ec816a6ecb84f70a8c
ConstantineMarkov <maga.ot.oz@protonmail.com>
2021-04-20 21:44:17 +0300

    Added selection sorting algorithm

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 5, 2, -1, 512, 5, 1, 4, 9, 18, -14, -154 };
+            
+            SelectionSort.Sort(array);
+            Console.WriteLine(string.Join(" | ", array));
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectionSort
+{
+    public static class SelectionSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            // First loop
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                // Get the min index which is i.
+                int minIndex = i;
+                
+                // Get the first element and set it as the min value.
+                T min = array[i];
+
+                // Loop through the rest of the elements starting from one index after the minimum one.
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    // If the next item has a lower value the minimum index is set to j.
+                    // Also the min value is set to the element with the min index from the array.
+                    if (array[j].CompareTo(min) < 0)
+                    {
+                        minIndex = j;
+                        min = array[j];
+                    }
+                }
+
+                // A temp variable holds the previous minimum value with 
+                // index of i - the last known min index in the scope of the outer loop.
+                T temp = array[i];
+
+                // The two values are swapped.
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
