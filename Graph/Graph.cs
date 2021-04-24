e66bcc1a421c0a4c30b2c2aeb933494779fa6745
ConstantineMarkov <maga.ot.oz@protonmail.com>
2021-04-24 11:01:52 +0300

    Added graph structure

---
+++ Graph/Graph.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+    public class Graph<T>
+    {
+        private bool _isDirected = false;
+        private bool _isWeighted = false;
+        public List<Node<T>> Nodes { get; set; } = new List<Node<T>>();
+
+        public Graph(bool isDirected, bool isWeighted)
+        {
+            _isDirected = isDirected;
+            _isWeighted = isWeighted;
+        }
+
+        public Edge<T> this[int from, int to]
+        {
+            get
+            {
+                Node<T> nodeFrom = Nodes[from];
+                Node<T> nodeTo = Nodes[to];
+
+                int i = nodeFrom.Neighbours.IndexOf(nodeTo);
+
+                if (i >= 0)
+                {
+                    Edge<T> edge = new Edge<T>()
+                    {
+                        From = nodeFrom,
+                        To = nodeTo,
+                        Weight = i < nodeFrom.Weights.Count ? nodeFrom.Weights[i] : 0
+                    };
+                    return edge;
+                }
+
+                return null;
+            }
+        }
+
+        public Node<T> AddNode(T value)
+        {
+            Node<T> node = new Node<T>() { Data = value };
+            Nodes.Add(node);
+            UpdateIndices();
+            return node;
+        }
+
+        public void RemoveNode(Node<T> nodeToRemove)
+        {
+            Nodes.Remove(nodeToRemove);
+            UpdateIndices();
+            foreach (Node<T> node in Nodes)
+            {
+                RemoveEdge(node, nodeToRemove);
+            }
+        }
+
+        public void AddEdge(Node<T> from, Node<T> to, int weight = 0)
+        {
+            from.Neighbours.Add(to);
+            if (_isWeighted)
+            {
+                from.Weights.Add(weight);
+            }
+
+            if (!_isDirected)
+            {
+                to.Neighbours.Add(from);
+
+                if (!_isDirected)
+                {
+                    to.Neighbours.Add(from);
+
+                    if (_isWeighted)
+                    {
+                        to.Weights.Add(weight);
+                    }
+                }
+            }
+        }
+
+        public void RemoveEdge(Node<T> from, Node<T> to)
+        {
+            int index = from.Neighbours.FindIndex(x => x == to);
+            if (index >= 0)
+            {
+                from.Neighbours.RemoveAt(index);
+            }
+        }
+
+        public List<Edge<T>> GetEdges()
+        {
+            List<Edge<T>> edges = new List<Edge<T>>();
+
+            foreach (Node<T> from in Nodes)
+            {
+                for (int i = 0; i < from.Neighbours.Count; i++)
+                {
+                    Edge<T> edge = new Edge<T>()
+                    {
+                        From = from,
+                        To = from.Neighbours[i],
+                        Weight = i < from.Weights.Count ? from.Weights[i] :
+                    };
+
+                    edges.Add(edge);
+                }
+            }
+            return edges;
+        }
+
+        private void UpdateIndices()
+        {
+            int i = 0;
+            Nodes.ForEach(x => x.Index = i++);
+        }
+    }
+}
