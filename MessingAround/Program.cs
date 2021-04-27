3fb588f51686e70861a1afc7da57da2363ebe45d
ConstantineMarkov <maga.ot.oz@protonmail.com>
2021-04-27 06:25:38 +0300

    Added some more practice I did

---
+++ MessingAround/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MessingAround
+{
+    class Program
+    {
+
+        // Stack overflow
+        static void Main(string[] args)
+        {
+            MyDict<int, string> myDict = new MyDict<int, string>();
+
+            myDict.Add(1, "rand1");
+            myDict.Add(2, "rand2");
+            myDict.Add(3, "rand3");
+            myDict.Add(4, "rand4");
+            myDict.Add(5, "rand5");
+            myDict.Add(6, "rand6");
+            myDict.Add(7, "rand7");
+            myDict.Add(8, "rand8");
+            myDict.Add(9, "rand9");
+            myDict.Add(10, "rand10");
+            myDict.Add(11, "rand11");
+            myDict.Add(12, "rand12");
+
+            foreach (string vale in myDict)
+            {
+                Console.WriteLine(myDict.ToString());
+            }
+        }
+    }
+
+    public interface IDict<TKey, TValue>
+    {
+        public TKey Get(TKey param);
+        public void Add(TKey key, TValue value);
+    }
+
+    public class MyDict<GKey, GValue> : IDict<GKey, GValue>, IEnumerable
+    { 
+        public MyDict<GKey, GValue> Dictionary { get; set; }
+
+        public MyDict()
+        {
+            Dictionary = new MyDict<GKey, GValue>();
+        }
+
+        public GKey Get(GKey key)
+        {
+            return Dictionary.Get(key);
+        }
+
+        public void Add(GKey key, GValue value)
+        {
+            Dictionary.Add(key, value);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Dictionary.GetEnumerator();
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter1, letter2, letter3;
+
+            letter1 = char.Parse(Console.ReadLine());
+            letter2 = char.Parse(Console.ReadLine());
+            letter3 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0}, {1}, {2}", letter3, letter2, letter1);
+
+            Console.WriteLine("Next values - a number and width");
+            int num = int.Parse(Console.ReadLine());
+
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= width; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+            // Decimal to binary
+
+
+            Console.WriteLine("Insert a decimal number");
+
+            int inp = int.Parse(Console.ReadLine());
+            string result = "";
+
+            while (inp > 1)
+            {
+                int reminder = inp % 2;
+
+                result = Convert.ToString(reminder) + result;
+
+                inp /= 2;
+            }
+
+            result = Convert.ToString(num) + result;
+
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
